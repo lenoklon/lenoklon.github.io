8a9a6dcc4145a7865022b66153cc726df43164e8
Olena L <lenok.lon@mail.ru>
2016-05-16 00:31:46 +0300

    ..

--- JS_1_2/2/script.js
+++ JS_1_2/2/script.js
@@ -1,28 +1,35 @@
-var arrName = [];
+var arrUsers = [];
+var user;
 
 var i = 0;
 
 while (i<5) {
-	var Name = prompt('add the ' + (i + 1) + 'name', '');
-	arrName.push(Name);
+	var user = prompt('add the ' + (i + 1) + ' name', '');
+	arrUsers.push(user);
 	i++;
 }
-console.log(arrName);
+
+console.log(arrUsers); // выведем наш массив имен
 
 var yourName = prompt('type your name here', '');
 
-// var rightName = false; //создаем флаг
+var flag = false; // default
 
-for (var i = 0; i < arrName.length; i++) {
+// проходимся по массиву
+for (var i = 0; i < arrUsers.length; i++) {
 
-	if (arrName[i] == yourName) {
-		// rightName = true;
-		alert('welcome to your profile ' + yourName);
+	if (arrUsers[i] === yourName) {
+		flag = true;
 		break;
-	} else if ((i == arrName.length - 1) && (arrName[i] != yourName)){ // (!rightName) сводим вывод ошибки только до посл эл
-		alert('sorry, first you should make registration');
-	}
+	} 
 }
+
+if ( flag ) {
+	alert('welcome to your profile ' + yourName);
+} else {
+	alert('sorry, first you should make registration');
+}
+
 	
 
 
