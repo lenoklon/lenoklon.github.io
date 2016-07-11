e2001f514c6f4b69b10a7c6d1bfb00129ea4f066
Olena L <lenok.lon@mail.ru>
2016-07-11 21:59:31 +0300

    ...

---
+++ TestAufgabe_JLX_LonskaOV/js/script.js
@@ -0,0 +1,11 @@
+// $(function () {
+// 	$('.header__nav li').hover (
+// 		function () {
+// 			$(this).children('ul').slideDown(150);
+// 		},
+// 		function () {
+// 			$('ul', this).slideUp(150); // или так
+// 		}
+
+// 	);
+// }
