bc990da5696c94473b38687749c224278a04f728
Olena L <lenok.lon@mail.ru>
2016-08-09 00:02:59 +0300

    insgesamt ohne slider3D

--- HAMBURGER/js/script.js
+++ HAMBURGER/js/script.js
@@ -1,67 +1,17 @@
 
-$(document).ready(function () {
-	// $('.header-menu').slicknav();
-	$('.slider').slick({
-		dots:true
+ $(function() {
+	$('.feedback__slide_wrap').slick({
+		// arrows:false
 	});
 
-	$('.clients__slider').slick({
-	  arrows:false,	
-	  infinite: true,
-	  speed: 300,
-	  slidesToShow: 6,
-	  slidesToScroll: 1,
-	  responsive:[
-	  	{
-	  		breakpoint:1200,  /*ноу меня почему-то 1080 а не 1200*/
-	  		settings: {
-	  			slidesToShow:5
-	  		}
-	  	},
-	  	{
-	  		breakpoint:992,  
-	  		settings: {
-	  			slidesToShow:4
-	  		}
-	  	},
-	  	{
-	  		breakpoint:769,  
-	  		settings: {
-	  			slidesToShow:3
-	  		}
-	  	},
-	  	{
-	  		breakpoint:769,  
-	  		settings: {
-	  			slidesToShow:2
-	  		}
-	  	},
-
-	  	{
-	  		breakpoint:480,  
-	  		settings: {
-	  			slidesToShow:1
-	  		}
-	  	}
-
-	  ]
+	$('.header__menu').click(function() {
+		$('.header__top_nav-list').fadeToggle(400);
+		// $('.header__top_nav-list').slideToggle(400);
 	});
 
-	$('.clients__prev').click(function(){
-		$('.clients__slider').slick('slickPrev');
-	});
 
-	$('.clients__next').click(function(){
-		$('.clients__slider').slick('slickNext');
+	$(".fa-search").click(function() {
+		$("form").fadeToggle(400);
 	});
-
-	$(function() {
-    $('#dg-container').gallery();
-    $('.header__nav_list').slicknav({
-    	prependTo:'.header__nav'
-    });
-});
 
 });
-
-/*спросить по поводу центрирования блоков контента в слайдере....есть ли какие-то логичные методы, а не методом тыка*/
