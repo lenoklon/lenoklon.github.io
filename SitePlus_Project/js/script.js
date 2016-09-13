7cc79e4239f78d20c65aa1291a630bef8035be06
Olena L <lenok.lon@mail.ru>
2016-09-13 21:14:06 +0300

    withou sliders

---
+++ SitePlus_Project/js/script.js
@@ -0,0 +1,18 @@
+
+ $(function() {
+	
+	$('.header__menu').click(function() {
+		$('.header__top_nav_list').fadeToggle(400);
+	});
+
+
+	$(".fa-search").click(function() {
+		$("form").fadeToggle(400);
+	});
+
+	// $('.sponsors__list').slick();
+
+
+
+
+});
