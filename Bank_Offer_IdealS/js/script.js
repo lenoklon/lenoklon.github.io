1bb03d09e0d7ef37821b9f1b0aa604b02171ee7c
Olena L <lenok.lon@mail.ru>
2016-09-14 23:11:32 +0300

    structure

--- Bank_Offer_IdealS/js/script.js
+++ Bank_Offer_IdealS/js/script.js
@@ -2,24 +2,9 @@
  $(function() {
 
  	$('.transfer_hide').hide();
-	
 	$('.main__left_list_item').click(function(e) {
-	
 		$(this).children('.transfer_hide').fadeToggle(400);
-
 	});
-
-
-	// $('.card_hide').hide();
-	
-	// $('.item__card').click(function(e) {
-	
-	// 	$('.card_hide').fadeToggle(400);
-
-	// });
-
-	
-
 });
 
 
