3f89add2ad87d7a5d8249a583de22dc7bb04a194
Olena L <lenok.lon@mail.ru>
2016-10-11 23:26:03 +0300

    fancybox

--- KERUGI/mail.php
+++ KERUGI/mail.php
@@ -1,7 +1,6 @@
 <?php
 
 $recepient = "mail@el.de";
-$sitename = "Название сайта";
 
 $name = trim($_GET["name"]);
 $UrName = trim($_GET["UrName"]);
@@ -9,6 +8,5 @@ $mail = trim($_GET["mail"]);
 $phone = trim($_GET["phone"]);
 $text = trim($_GET["text"]);
 
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
 $message = "Имя: $name \nТелефон: $phone \nТекст: $text";
-mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+mail($recepient, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
