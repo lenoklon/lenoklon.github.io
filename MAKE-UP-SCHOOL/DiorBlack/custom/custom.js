b2d77738fbc95eb3c93c08b79e65c44a966cff80
Olena L <lenok.lon@mail.ru>
2017-02-06 00:02:59 +0200

    not linked but finished

--- MAKE-UP-SCHOOL/DiorBlack/custom/custom.js
+++ MAKE-UP-SCHOOL/DiorBlack/custom/custom.js
@@ -16,49 +16,4 @@ $(document).ready(function() {
     $('#stars2').starrr({
         rating: 0
     });
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        slidesPerView: 4,
-        paginationClickable: true,
-        speed: 300,
-        slidesOffsetAfter: 17,
-        slidesOffsetBefore: 17,
-        nextButton: '.swiper-button-next2',
-        prevButton: '.swiper-button-prev2',
-        spaceBetween: 5,
-//        centeredSlides: true,
-        loop: true,
-        breakpoints: {
-            1980: {
-                slidesPerView: 4,
-                slidesOffsetBefore: 30,
-                centeredSlides: false
-            },
-            1439: {
-                slidesPerView: 3,
-                slidesOffsetBefore: 30,
-                centeredSlides: true
-            },
-            1200: {
-                slidesPerView: 2,
-                slidesOffsetBefore: 30
-                , centeredSlides: false
-            },
-            991: {
-                slidesPerView: 3,
-                slidesOffsetBefore: 30,
-                centeredSlides: false
-            },
-            426: {
-                slidesPerView: 2,
-                slidesOffsetBefore: 12,
-                centeredSlides: false
-            },
-            376: {
-                slidesPerView: 1,
-                slidesOffsetBefore: 10,
-                centeredSlides: true
-            }
-        }
-    });
 });
--- MAKE-UP-SCHOOL/DiorBlue/custom/custom.js
+++ MAKE-UP-SCHOOL/DiorBlue/custom/custom.js
@@ -16,54 +16,5 @@ $(document).ready(function() {
     $('#stars2').starrr({
         rating: 0
     });
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        slidesPerView: 4,
-        paginationClickable: true,
-        speed: 300,
-        slidesOffsetAfter: 17,
-        slidesOffsetBefore: 17,
-        nextButton: '.swiper-button-next2',
-        prevButton: '.swiper-button-prev2',
-        spaceBetween: 5,
-//        centeredSlides: true,
-        loop: true,
-        breakpoints: {
-            1980: {
-                slidesPerView: 4,
-                slidesOffsetBefore: 30,
-                centeredSlides: false
-            },
-            1439: {
-                slidesPerView: 3,
-                slidesOffsetBefore: 30,
-                centeredSlides: true
-            },
-            1200: {
-                slidesPerView: 2,
-                slidesOffsetBefore: 30
-                , centeredSlides: false
-            },
-            991: {
-                slidesPerView: 3,
-                slidesOffsetBefore: 30,
-                centeredSlides: false
-            },
-            660: {
-                slidesPerView: 2,
-                slidesOffsetBefore: 30,
-                centeredSlides: false
-            },
-            426: {
-                slidesPerView: 1,
-                slidesOffsetBefore: 12,
-                centeredSlides: false
-            },
-            376: {
-                slidesPerView: 1,
-                slidesOffsetBefore: 10,
-                centeredSlides: true
-            }
-        }
-    });
+    
 });
