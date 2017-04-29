2d08c7223c113462e4a7b2c9f6e00bea1f3f81d0
Olena L <lenok.lon@mail.ru>
2017-04-29 21:19:25 +0300

    crop

--- Luxoft_Test/js/common.js
+++ Luxoft_Test/js/common.js
@@ -19,11 +19,22 @@ $(document).ready(function() {
 			for(var i in data) {
 				var newsItem = data[i];
 			
+				/*Cropping text*/
+				if(newsItem.text.length > 350) {
+					//cropping
+					newsItem.croppedText = newsItem.text.substr(0, 350); /*!!! почему мы не обьявляем эту переменную cropped*/
+					newsItem.croppedText += ' ...';
+				} else {
+					newsItem.croppedText = newsItem.text; /*to avoid deleting info*/
+				}
 
 				var template = $($('.news-template').html());
 				template.find('.news-title').text(newsItem.title);
 				template.find('.news-date').text(newsItem.date);
-				template.find('.news-text').text(newsItem.text);
+				template.find('.news-text').data('text', newsItem.text)
+				.data('croppedText', newsItem.croppedText)
+				.text(newsItem.croppedText);
+
 				$('.main-news').append(template);
 			}
 		}
